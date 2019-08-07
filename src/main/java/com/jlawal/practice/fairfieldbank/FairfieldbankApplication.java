d25549725a3737e537d1ae67651187606b793d6a
JohnLawal <jilawal@mum.edu>
2019-08-06 22:36:44 -0500

    first setup of project structure

---
+++ src/main/java/com/jlawal/practice/fairfieldbank/FairfieldbankApplication.java
@@ -0,0 +1,13 @@
+package com.jlawal.practice.fairfieldbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairfieldbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FairfieldbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jlawal/practice/fairfieldbank/FairfieldbankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jlawal.practice.fairfieldbank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FairfieldbankApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
